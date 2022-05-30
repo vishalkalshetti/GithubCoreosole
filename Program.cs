b9afeb910aba90a4a7ac2080631994c20125d1eb
Vishwanath Kalshetty <Vishalkalshetti2@gmail.com>
2022-05-30 12:33:51 +0530

    Added

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GithubCoreosole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Github Added");
         }
     }
 }
